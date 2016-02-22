d81c30e1ef8c380cbeaa41119e545670b6d00e9b
Hunter Marshall <hjmarshall18@gmail.com>
2016-02-22 08:45:06 -0700

    Added Unisversity Store Logo to title bar.

--- app/src/main/java/com/example/hunter/scanr/Home.java
+++ app/src/main/java/com/example/hunter/scanr/Home.java
@@ -1,5 +1,6 @@
 package com.example.hunter.scanr;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // gets the action bar for the application
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.bstore_logo);
     }
 }
