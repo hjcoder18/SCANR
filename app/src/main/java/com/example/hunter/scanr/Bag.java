bbe6e054cf2a0ad3fc81e892ea4d5843f6791323
Hunter Marshall <hjmarshall18@gmail.com>
2016-02-24 08:50:06 -0700

    Started adding methods in Shelf class, and variables in bag class.

--- app/src/main/java/com/example/hunter/scanr/Bag.java
+++ app/src/main/java/com/example/hunter/scanr/Bag.java
@@ -1,7 +1,14 @@
 package com.example.hunter.scanr;
 
+import java.util.List;
+
 /**
  * Created by Hunter on 2/24/2016.
  */
 public class Bag {
+    List<Book> books;
+    int bagId;
+    String studentName;
+    int studentID;
+
 }
--- app/src/main/java/com/example/hunter/scanr/Shelf.java
+++ app/src/main/java/com/example/hunter/scanr/Shelf.java
@@ -1,8 +1,26 @@
 package com.example.hunter.scanr;
 
+import java.util.List;
+
 /**
  * Created by Hunter on 2/24/2016.
  */
 public class Shelf {
+    List<Bag> bags;
+    String room;
+    int roomID;
 
+    void addBag(Bag bag) {
+        bags.add(bag);
+    }
+
+    // NOT SURE WE NEED THIS
+    void removeBag() {
+        // are we assuming the API is perfect with no errors?
+    }
+
+    // NOT SURE WE NEED THIS
+    void display() {
+
+    }
 }
