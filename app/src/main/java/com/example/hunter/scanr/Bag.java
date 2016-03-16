37a7402213348a7727becf284d0ca896530795c8
Korey MacGill <kmmacgill@gmail.com>
2016-03-16 08:46:30 -0600

    adding proper javadoc documentation comments for Bag class

--- app/src/main/java/com/example/hunter/scanr/Bag.java
+++ app/src/main/java/com/example/hunter/scanr/Bag.java
@@ -8,7 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Hunter on 2/24/2016.
+ * The Bag Class, represents each bag on a rack
+ *
+ * The bags are being checked on each rack, as such the bag class represents
+ * the information being held by each bag, namely the students information
+ * book information has been excluded as this is outside the scope of this
+ * program.
+ *
+ * @author Korey MacGill, Hunter Marshall, William Montesdeoca
+ * @version 2016.0215
+ * @since 1.0
  */
 public class Bag extends AppCompatActivity{
 
@@ -32,7 +41,7 @@ public class Bag extends AppCompatActivity{
     private String studentLastName;  // used by bag class
     private int studentID;
     private int aisle;
-    private String shelfID; //
+    private String rackID; //
     //private int bagIndex;  // used by Shelf class
 
     /*88888888888888888888888888888888888888888888888888888888888888888
@@ -41,75 +50,115 @@ public class Bag extends AppCompatActivity{
         - get first bag student's last name
         - get last bag student's last name
     8888888888888888888888888888888888888888888888888888888888888888888*/
+
+    /**
+     * Default Constructor
+     */
     public Bag() {
         //bagIndex = -1;
     }
 
-    //non-default constructor
+    /**
+     * non-default constructor
+     *
+     * @param id - the unique identifier of the bag.
+     */
     public Bag(String id) {
         bagId = id;
     }
 
-    //add a book to the list of books
-    public void addBook(Book book) {
-        try {
-            books.add(book);
-        } catch(Exception e) {
-            Log.d(TAG, "ERROR: Did not add the book to bag!", e);
-        }
-
-    }
-
-    //STUDENT NAME getters and setters
+    /**
+     * Method that sets the student's name associated with the bag.
+     *
+     * @param firstName - the first name of the student
+     * @param lastName - the last name of the student
+     */
     public void setStudentName (String firstName, String lastName) {
         studentFirstName = firstName;
         studentLastName = lastName;
         Log.i(TAG, "This will set the name of the student.");
     }
+
+    /**
+     * The accessor for retrieving the student's first and last name
+     *
+     * @return name - a string of the students first and last name
+     */
     public String getStudentName() {
         String name = studentLastName + ", " + studentFirstName;
         return name;
     }
 
-    //STUDENT ID getters and setters
+    /**
+     * The student id mutator method
+     *
+     * @param id - the id that the student's id will be set to.
+     */
     public void setStudentID (int id) {
         studentID = id;
     }
+
+    /**
+     * Accessor method for retrieving a students ID
+     *
+     * @return studentID - returns the students ID
+     */
     public int getStudentID() {
         return studentID;
     }
 
-
-    //ID getters and setters
+    /**
+     * mutator method for bag's ID
+     *
+     * @param id - the id the bag's ID will be set to.
+     */
     public void setBagId (String id) {
         bagId = id;
     }
+
+    /**
+     * Accessor Method for retrieving the bag ID
+     *
+     * @return bagId - the bag's id
+     */
     public String getBagId() {
         return bagId;
     }
 
-    //return the number of books in the bag...
-    public double numberOfBooks() {
-        return books.size();
-    }
-
-//    public void setIndex (int ind) { bagIndex = ind; }
-//    public int getIndex() { return bagIndex; }
-
-    //AISLE getters and setters
+    /**
+     * Mutator method to set the Aisle location of the bag
+     *
+     * @param pos - the position of the bag for its respective Aisle
+     */
     public void setAisle(int pos) {
         this.aisle = pos;
     }
+
+    /**
+     * The accessor method to retrieve the Aisle location of a bag
+     *
+     * @return aisle - the aisle location of the bag.
+     */
     public int getAisle() {
         return aisle;
     }
 
-    //SHELF getters and setters
-    public void setShelf(String shelf) {
-        shelfID = shelf;
+    /**
+     * the mutator method to set and change the Rack ID of the bag
+     *
+     * @param rack - the rack to set the bag's rackID to
+     */
+    public void setRack(String rack) {
+        rackID = rack;
     }
-    public String getShelfID() {
-        return shelfID;
+
+    /**
+     * Accessor method to retrieve the rackID of the bag
+     *
+     * @return - rackID - the rackID of the bag
+     */
+    public String getRackID() {
+        return rackID;
     }
 
 }
